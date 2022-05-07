c38dc64ebfe8b312075cb55d728add83a8bbd7ef
joaquincorimayo <joaquin97corimayo@gmail.com>
2022-05-07 12:07:01 -0300

    Se agrega contenido en beneficios. Modificacion del navbar.

--- TP5_Grupo10/src/main/java/ar/edu/unju/fi/tp5/controller/BecaController.java
+++ TP5_Grupo10/src/main/java/ar/edu/unju/fi/tp5/controller/BecaController.java
@@ -1,8 +1,13 @@
 package ar.edu.unju.fi.tp5.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 
 @Controller
 public class BecaController {
-
+	
+	public String getBeca(Model model) {
+		
+		return "";
+	}
 }
